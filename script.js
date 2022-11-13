4355d5f1e56bfe1652d79bfeb7322937f4873dcf
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2022-11-12 21:25:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+//Assign Code
+
 let btnGenerate = document.querySelector('button');
 let result = document.querySelector('h1');
 
+//Add Event Listtener for Button
 btnGenerate.addEventListener('click', ()=> {
   result.innerText = generatePassword();
 });
+
+//Write Password
+function writePassword(){
+  var newPassword = generatePassword();
+  
+  passwordTest.value ="";
+  function generatePassword(){
+    var password ="";
+  }
+  
+  return password
+}
