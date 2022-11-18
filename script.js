915dd1119a6bf3396f508cb10225dfd8c63989cc
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2022-11-18 00:43:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,121 @@
-//assign code
-let btnGenerate = document.querySelector('button');
-let result = document.querySelector('h1');
+  //Password Generator 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-btnGenerate.addEventListener('click', ()=> {
-  result.innerText = generatePassword();
-});
+// Write password to the #password input
+function writePassword() {
+   var passwordText = document.querySelector("#password");
 
-<body>  
-<h3>Password Validation</h3>
-<p>Try to submit the form.</p>
+  //  Creates a variable for the password character length and window pop up 
+  // for user to enter desired character length
+  let length = window.prompt('Enter Password Length (8 - 128)');
 
-<div class="container">
-    <label for="psw">Password</label>
-</div>
+// While loop to check that character  
+//Also makes sure that a number is entered into the box with isNaN 
+  while (isNaN(length) || length < 8 || length > 128) {
+    length = window.prompt('Only enter numbers between 8 and 128!');
+  }
+  // sets variables for users to select for password to false to help with logic further down page
+  let typesSelected = false;
+  let includeUpperCase = false;
+  let includeLowerCase = false;
+  let includeNumerics = false;
+  let includeSpecials = false;
 
-<div id="message">
-  <h3>Password must contain the following:</h3>
-  <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-  <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-  <p id="number" class="invalid">A <b>number</b></p>
-  <p id="length" class="invalid">Minimum <b>8 characters</b></p>
-</div>
-				
-
-<script>
-var myInput = document.getElementById("psw");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block"}
-
-
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() 
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {  
-    letter.classList.remove("invalid")}
-    letter.classList.add("valid");
-  {"}"} else
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-  {"}"}
+  // while loop to with window.confirm to allow user to select yes or no for password requirements
+  while (!typesSelected) {
+    includeUpperCase = window.confirm(
+      'Include Upper Case Characters? (OK=Yes, Cancel=No)'
+    );
   
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) 
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
+    includeLowerCase = window.confirm(
+      'Include Lower Case Characters? (OK=Yes, Cancel=No)'
+    );
+  
+    includeNumerics = window.confirm(
+      'Include Numeric Characters? (OK=Yes, Cancel=No)'
+    );
+  
+    includeSpecials = window.confirm(
+      'Include Special Characters? (OK=Yes, Cancel=No)'
+    );
+  
+// variable to declare that 'typesSelected' can be any of Uppercase, Lowercase, Numerics or Special Characters
+    typesSelected =   includeUpperCase || includeLowerCase || includeNumerics || includeSpecials;
+  
+// If statement to have window alert show to the user if they do not select at least one of the above character types 
+    if (typesSelected === false) {
+      window.alert('At least one character type must be included.');
+    }
+  }
+  
+  // setting the number length to the same as the password length criteria
+  let len = Number(length);
+  
+  // console logs for testing and debugging -- not showing on dployed site
+  // console.log(len);
+  // console.log(includeUpperCase);
+  // console.log(includeLowerCase);
+  // console.log(includeNumerics);
+  // console.log(includeSpecials);
+  
+// declaring the 'password' variable should include all of the following that the user selected and generated using the 'generatePassword' Function
+  let password = generatePassword(len, includeUpperCase, includeLowerCase, includeNumerics, includeSpecials);
+  
+  passwordText.value = password;
 
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) 
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-</script>
-</body>
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Function to generate the user's password using the selected source characters and randomizing the characters
+function generatePassword(len, includeUpper, includeLower, includeNumeric, includeSpecial) {
+
+  // variable to set source characters that the user wil choose below
+  let sourceCharacters = '';
+
+  // If statements set source characters if user selects uppercase characters for password
+  if (includeUpper) {
+    sourceCharacters +='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+
+
+  // If statements set source characters if user selects lowercase characters for password
+  if (includeLower) {
+    sourceCharacters +='abcdefghijklmnopqrstuvwxyz';
+  }
+
+
+  // If statements set source characters if user selects numerica characters for password
+  if (includeNumeric) {
+    sourceCharacters +='01234567890';
+  }
+
+
+  // If statements set source characters if user selects special characters for password
+  if (includeSpecial) {
+    sourceCharacters +='!"#$%&*+,-./:;<=>?@[\]^_`{|}~';
+  }
+
+// variable set to return the password 
+  let returnPassword = '';
+
+// for loop set up to randomize characters that the user selects by answering the questions.  It will also set it to the requested length by the user
+  for (let i = 0; i < len; i++) {
+
+    let sourceLength = sourceCharacters.length;
+    let charPos = Math.floor(Math.random() * sourceLength);
+    console.log(charPos);
+    let newChar = sourceCharacters.substring(charPos, charPos + 1);
+
+    console.log(newChar);
+
+  returnPassword += newChar;
+
+  }
+// returns password functions
+  return returnPassword;
+
+}
